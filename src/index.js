f775e3ed4ec2c4344c4818844ceebc3fe9a4d6e1
Ihar Piatrenka <iharpiatrenka@Ihars-iMac.local>
2020-01-14 15:02:58 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (Number.isNaN(n)) return NaN;
+    let s = "";
+    let w = Math.abs(n);
+    while (w > 0) {
+        s += (w % 10).toString();
+        w = Math.floor(w / 10);
+    }
+    return parseInt(s);
+};
